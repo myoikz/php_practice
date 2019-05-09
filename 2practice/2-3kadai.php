44ad0f211e66e814acd13e07589556baa36628dc
oike <oike.kazuki.uz@mynavi.jp>
2019-05-09 05:18:05 +0000

    PHP/Laravel 03 プログラムの実行を条件分岐構文と繰り返し構文で制御できるようになろうの課題

---
+++ 2practice/2-3kadai.php
@@ -0,0 +1,11 @@
+<?php
+/*$fruits に配列で好きなフルーツを5個代入し、foreach文で順番に出力してください。*/
+
+$fruits = ["apple", "pineapple", "peach", "orange", "banana"];
+
+foreach($fruits as $fruit){
+    echo $fruit;
+    echo "\n";
+}
+
+?>
